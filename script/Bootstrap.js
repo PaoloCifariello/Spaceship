a6239fe79d51eff71946068e42c40d7f3d91b230
Piellekappa <paolocifa@gmail.com>
2013-06-30 16:55:55 +0200

    parametric screen position

--- script/Bootstrap.js
+++ script/Bootstrap.js
@@ -16,9 +16,15 @@ function Colpo(posx, posy, id)
 
 function InitializeShip(ship, id)
 {
+    ship.style.position = 'absolute';
+    ship.style.left = GetWidth() / 2 + 'px';
+    ship.style.margin = "0px";
+    ship.style.width = '150px';
+    ship.style.height = '100px';
+
     if (id == 1) 
     {
-        ship.style.top = "600px";
+        ship.style.top = ( GetHeight() - parseInt( ship.style.height ) ) + 'px';
     }
     
     else 
@@ -27,17 +33,47 @@ function InitializeShip(ship, id)
     }
 
     ship.class = "char";
-
-    ship.style.position = 'absolute';
-    ship.style.left = '600px';
-    ship.style.margin = "0px";
-    ship.style.width = '150px';
-    ship.style.height = '100px';
     
     ship.onclick = "";
     ship.onmouseover = "";
     ship.onmouseout = "";
 }
+
+ function GetWidth()
+  {
+          var x = 0;
+          if (self.innerHeight)
+          {
+                  x = self.innerWidth;
+          }
+          else if (document.documentElement && document.documentElement.clientHeight)
+          {
+                  x = document.documentElement.clientWidth;
+          }
+          else if (document.body)
+          {
+                  x = document.body.clientWidth;
+          }
+          return x;
+  }
+
+  function GetHeight()
+  {
+          var y = 0;
+          if (self.innerHeight)
+          {
+                  y = self.innerHeight;
+          }
+          else if (document.documentElement && document.documentElement.clientHeight)
+          {
+                  y = document.documentElement.clientHeight;
+          }
+          else if (document.body)
+          {
+                  y = document.body.clientHeight;
+          }
+          return y;
+  }
 
 document.title = "Flight Game"
 document.getElementById('chooseTitle').innerText = 'Scegli la navicella 1:';
